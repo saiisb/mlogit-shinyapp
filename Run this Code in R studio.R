15a82af6045146db172de733ac645f749dfe0a62
saiisb <saichand_chintala@isb.edu>
2016-10-17 16:53:26 +0530

    Fixed the Source Warning message

--- Run this Code in R studio.R
+++ Run this Code in R studio.R
@@ -1,4 +1,4 @@
 
 
-source('https://github.com/saiisb/mlogit-shinyapp/blob/master/mlogit-dependency-shiny.R')
+source('https://raw.githubusercontent.com/saiisb/mlogit-shinyapp/master/mlogit-dependency-shiny.R')
 runGitHub('mlogit-shinyapp','saiisb')
